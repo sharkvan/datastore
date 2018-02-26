ae9f22457023f5636f8da06d8b8fe33786a78109
Tim Schruben <tim.schruben@gmail.com>
2018-02-25 23:45:25 -0500

    removing the dividend collection from the csv data stream

--- apps/webapi/web/controllers/stock_controller_csv.ex
+++ apps/webapi/web/controllers/stock_controller_csv.ex
@@ -5,6 +5,7 @@ defmodule Webapi.StockControllerCSV do
         conn 
         |> put_resp_content_type("text/plain")
         |> send_resp(200, Webapi.StockCache.Cache.stream()
+                                |> Stream.map(fn(m) -> Map.drop(m, ["dividends"]) end)
                                 |> CSV.Encoding.Encoder.encode(headers: true)
                                 |> Enum.to_list)
     end
